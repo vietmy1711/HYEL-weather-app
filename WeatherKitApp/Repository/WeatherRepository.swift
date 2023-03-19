4a582901eb83ebae231b0e2798f78770efafe1d9
My Nguyen <my.nguyen@navercorp.com>
2023-03-19 22:55:26 +0700

    Don't reuse cache if fetch hourly

--- WeatherKitApp/Repository/WeatherRepository.swift
+++ WeatherKitApp/Repository/WeatherRepository.swift
@@ -41,25 +41,15 @@ class WeatherRepository: ObservableObject {
         let dataset = "forecastHourly"
         ApiRequest.shared.request(path: "\(lat)/\(lon)", method: .GET, queryParams: ["dataSets": dataset, "dailyStart": day.forecastStart ?? ""]) { data, errType, error in
             guard let data = data else {
-                if let model = AppCache.shared.getWeatherModel() {
-                    completionHandler(model, errType)
-                    return
-                }
-
                 completionHandler(nil, errType)
                 return
             }
             do {
                 let jsonDecoder = JSONDecoder()
                 let weatherModel = try jsonDecoder.decode(WeatherModel.self, from: data)
-                AppCache.shared.setWeatherModel(weatherModel)
                 completionHandler(weatherModel, errType)
             } catch let parsingError {
                 print("Error: \(parsingError)")
-                if let model = AppCache.shared.getWeatherModel() {
-                    completionHandler(model, errType)
-                    return
-                }
                 completionHandler(nil, errType)
             }
         }
