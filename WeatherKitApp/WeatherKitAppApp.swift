96a83318ca84faaa5753ed4f11ed348488476c51
My Nguyen <my.nguyen@navercorp.com>
2023-03-19 14:48:37 +0700

    Initial Commit

---
+++ WeatherKitApp/WeatherKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKitAppApp.swift
+//  WeatherKitApp
+//
+//  Created by GJ376GXGQ0 on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
